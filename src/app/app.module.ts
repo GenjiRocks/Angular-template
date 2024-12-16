1f830d6b34ebbaf11095289a26a44b62ac30e1f1
Ashwin <ashwindeepak1997@gmail.com>
2024-12-16 17:57:58 +0530

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { TablesComponent } from './tables/tables.component';
+import { HeaderComponent } from './header/header.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    TablesComponent,
+    HeaderComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
